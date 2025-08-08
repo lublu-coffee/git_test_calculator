dc3464fcac57f887e7f875872e7a2ed19afe66d4
Tamiron Post <tdrozdikova@gmail.com>
2025-08-08 13:55:13 +0300

    ооп получение приватных чисел

--- object_calculator.py
+++ object_calculator.py
@@ -3,6 +3,22 @@ class Calculator:
         self.__a = a
         self.__b = b
 
+    @property
+    def number_1(self):
+        return self.__a
+
+    @number_1.setter
+    def number_1(self, a):
+        self.__a = int(a)
+
+    @property
+    def number_2(self):
+        return self.__b
+
+    @number_2.setter
+    def number_2(self, b):
+        self.__b = int(b)
+
     def plus(self):
         self.print_result('Сумма', self.__a + self.__b)
 
